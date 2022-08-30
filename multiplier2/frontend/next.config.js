00ef9f506f1cf845430927281b6d7774fc91ed55
netanmangal <imnetanmangal@gmail.com>
2022-08-31 00:11:58 +0530

    Add: will be disposed in next commit

--- multiplier2/frontend/next.config.js
+++ multiplier2/frontend/next.config.js
@@ -1,6 +1,44 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true
+// }
+
+// module.exports = nextConfig
+
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    ...defaultConfig,
+    reactStrictMode: true,
+    webpack: (config) => {
+      // config.resolve = {
+      //   ...config.resolve,
+      //   fallback: {
+      //     "fs": false,
+      //     "path": false,
+      //     "os": false,
+      //   }
+      // },
+      config.module = {
+        ...config.module,
+        rules: [
+          {
+            ...config.module.rules[0],
+            test: /\.m?js/,
+            resolve: {
+                fullySpecified: false
+            },
+            // exclude: [/node_modules/],
+            loader: require.resolve('source-map-loader'),
+          },
+        ]
+      }
+      // config.module.rules.push({
+      //   exclude: [/node_modules/],
+      //   // loader: require.resolve('source-map-loader'),
+      // })
+
+      return config
+    },
+  }
 }
 
-module.exports = nextConfig
--- multiplier2/frontend/utils/verifyRequestor.js
+++ multiplier2/frontend/utils/verifyRequestor.js
@@ -1,5 +1,13 @@
-const verifyRequestor = (localState, setLocalState) => {
+const generateCalldata = require("../utils/generate_calldata.js");
+
+const verifyRequestor = async (localState, setLocalState) => {
     console.log(localState);
+    console.log("WAITING____");
+
+    let resp = await generateCalldata(localState.factor1);
+    console.log(resp);
+
+    console.log("WAITING_OVER____");
 }
 
 export default verifyRequestor;
