f9873fa843dcd57178a275a5f6af6fb94715e091
netanmangal <imnetanmangal@gmail.com>
2022-09-04 23:58:31 +0530

    Add: verifier request to contract

--- multiplier2/frontend/Components/Card.js
+++ multiplier2/frontend/Components/Card.js
@@ -1,71 +1,94 @@
 import { useState } from 'react';
+import {
+  useConnect,
+  useAccount,
+  useNetwork,
+  useContract,
+  useSigner,
+  useProvider
+} from "wagmi";
 
 import styles from '../styles/Home.module.css';
 import detailStyles from '../styles/Details.module.css';
 import verifyRequestor from '../utils/verifyRequestor.js';
+import { switchNetwork } from "../utils/switchNetwork.js";
+import networks from "../utils/networks.json";
+import multiplierContractAbi from "../utils/abiFiles/Multiplier.json";
+import contractAddress from "../utils/contractAddress.json";
 
 const Card = () => {
-    let [localState, setLocalState] = useState({
-        name: "",
-        factor1: "",
-        factor2: ""
-    });
+  let [localState, setLocalState] = useState({
+    name: "",
+    factor1: "",
+    factor2: ""
+  });
 
-    return (
-        <div className={detailStyles.bigCard}>
-            <p>Prove you know the <b>Factors of 33.</b>
-                <br />
-                Don't worry, it's a <b>Trusted</b> setup. We ensure <b>ZeroKnowledge</b> transfer.</p>
-            <p>Details:</p>
+  const [connectQuery, connect] = useConnect();
+  const [accountQuery, disconnect] = useAccount();
+  const [{ data, error, loading }] = useNetwork();
+  const [signer] = useSigner();
+  const provider = useProvider();
+  const contract = useContract({
+    addressOrName: contractAddress.multiplier2Contract,
+    contractInterface: multiplierContractAbi.abi,
+    signerOrProvider: signer.data || provider,
+  });
 
-            <input
-                type={"string"}
-                className={styles.input}
-                placeholder="Name"
-                value={localState.name}
-                onChange={(e) => {
-                    setLocalState({
-                        ...localState,
-                        name: e.target.value
-                    })
-                }}
-            />
+  return (
+    <div className={detailStyles.bigCard}>
+      <p>Prove you know the <b>Factors of 33.</b>
+        <br />
+        Don't worry, it's a <b>Trusted</b> setup. We ensure <b>ZeroKnowledge</b> transfer.</p>
+      <p>Details:</p>
 
-            <input
-                type={"number"}
-                className={styles.input}
-                placeholder="Factor 1"
-                value={localState.factor1}
-                onChange={(e) => {
-                    setLocalState({
-                        ...localState,
-                        factor1: e.target.value
-                    })
-                }}
-            />
+      <input
+        type={"string"}
+        className={styles.input}
+        placeholder="Name"
+        value={localState.name}
+        onChange={(e) => {
+          setLocalState({
+            ...localState,
+            name: e.target.value
+          })
+        }}
+      />
 
-            <input
-                type={"number"}
-                className={styles.input}
-                placeholder="Factor 2"
-                value={localState.factor2}
-                onChange={(e) => {
-                    setLocalState({
-                        ...localState,
-                        factor2: e.target.value
-                    })
-                }}
-            />
+      <input
+        type={"number"}
+        className={styles.input}
+        placeholder="Factor 1"
+        value={localState.factor1}
+        onChange={(e) => {
+          setLocalState({
+            ...localState,
+            factor1: e.target.value
+          })
+        }}
+      />
 
-            <div className={detailStyles.fotter}>
-                <button className={styles.gradientButton} onClick={() => {
-                    verifyRequestor(localState, setLocalState);
-                }}>
-                    Check, If I Know !!!
-                </button>
-            </div>
-        </div>
-    );
+      <input
+        type={"number"}
+        className={styles.input}
+        placeholder="Factor 2"
+        value={localState.factor2}
+        onChange={(e) => {
+          setLocalState({
+            ...localState,
+            factor2: e.target.value
+          })
+        }}
+      />
+
+      <div className={detailStyles.fotter}>
+        <button className={styles.gradientButton} onClick={() => {
+          verifyRequestor(localState, setLocalState, contract);
+        }}>
+          Check, If I Know !!!
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- multiplier2/frontend/utils/verifyRequestor.js
+++ multiplier2/frontend/utils/verifyRequestor.js
@@ -1,12 +1,20 @@
-const { multiplier2CallData } = require("../zkproof/multiplier2/snarkjsMultiplier2.js");
+import { multiplier2CallData } from "../zkproof/multiplier2/snarkjsMultiplier2.js";
 
-const verifyRequestor = async (localState, setLocalState) => {
-    console.log(localState);
+const verifyRequestor = async (localState, setLocalState, contract) => {
     console.log("WAITING____");
 
-    let resp = await multiplier2CallData(localState.factor1, localState.factor2);
-    console.log(resp);
+    let calldata = await multiplier2CallData(localState.factor1, localState.factor2);
+    console.log(calldata);
 
+    let resp = await contract.iKnowMultipliers(
+        localState.name,
+        calldata[0],
+        calldata[1],
+        calldata[2],
+        calldata[3]
+    );
+
+    console.log(resp);
     console.log("WAITING_OVER____");
 }
 
